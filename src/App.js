c8811ae2f44ec3907283f453dd96b60ebcd730d6
jhonaquejadau <jhona.quejada@gmail.com>
2023-01-08 18:51:03 -0500

    redux implemented

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
 import React from "react";
+import {legacy_createStore } from "redux";
 
-export default function App () {
-    return (
-        <h1>HELLOOO</h1>
-    )
+function counterReducer(state = 0, action) {
+  switch (action.type){
+      case '@counter/increment':
+          return state + 1
+      case '@counter/decrement':
+          return state - 1
+      case '@counter/reset':
+          return 0
+      default:
+          return state
+  }
+}
+
+const incrementValue = {
+  type: "@counter/increment",
+};
+
+const decrementValue = {
+  type: "@counter/decrement",
+};
+
+const resetValue = {
+  type: "@counter/reset",
+};
+
+const store = legacy_createStore(counterReducer)
+store.subscribe(() => console.log(store.getState()))
+
+export default function App() {
+  return (
+    <div>
+      <h1>STATE VALUE</h1>
+      <h2>{store.getState()}</h2>
+      <button onClick={() => store.dispatch(incrementValue)}>Add 1</button>
+      <button onClick={() => store.dispatch(resetValue)}>Reset</button>
+      <button onClick={() => store.dispatch(decrementValue)}>Subtract 1</button>
+    </div>
+  );
 }
